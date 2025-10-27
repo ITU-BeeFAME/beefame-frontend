ee27b103232c326daf4dcea9fd7c669b049986cf
Furkan Topaloğlu <furkan.topaloglu@cimri.com>
2025-10-27 23:57:17 +0300

    change url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Use the live backend URL instead of localhost
-const baseURL = 'http://api.beefair.site';
+const baseURL = 'https://api.beefair.site';
 
 export const api = axios.create({
   baseURL: baseURL,
